9740eafec92f2f4ca4ef669a0d32681fb33414e1
Nadir Zhumabaev <Nodirbek_zhumabaev@epam.com>
2018-07-06 14:33:49 +0300

    task/4 completed Filtering courses list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CourseItemEntity} from './course/course-item/course-item.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  private data;
+  public courseItems;
+
+  ngOnInit() {
+    this.data = CourseItemEntity.listCreator([
+      {
+        id: 0,
+        title: 'First Title',
+        creationDate: new Date(new Date().setDate(new Date().getDate() + 1)),
+        duration: 60,
+        description: 'some description',
+      },
+      // moved upper in order to see - how orderBy pipe works
+      {
+        id: 2,
+        title: 'Third Title',
+        creationDate: new Date(new Date().setDate(new Date().getDate() - 1)),
+        duration: 44,
+        description: 'Awesome description',
+      },
+      {
+        id: 1,
+        title: 'Second Title',
+        creationDate: new Date('2018-06-11'),
+        duration: 20,
+        description: 'Cool description',
+      },
+    ]);
+
+    this.courseItems = [...this.data];
+  }
+
+  public filterFunc = (search) => {
+    this.courseItems = [...this.data.filter(el => el.title.includes(search))];
+  }
+}
--- src/app/course/course-list/course-list.component.ts
+++ src/app/course/course-list/course-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import { CourseItemEntity } from '../course-item/course-item.component';
 
 @Component({
@@ -6,40 +6,11 @@ import { CourseItemEntity } from '../course-item/course-item.component';
   templateUrl: './course-list.component.html',
   styleUrls: ['./course-list.component.css']
 })
-export class CourseListComponent implements OnInit {
-  public courseItems: CourseItemEntity[] = [];
-
+export class CourseListComponent {
+  @Input() public items: CourseItemEntity[] = [];
   constructor() { }
 
   loadMoreCoursesHandler(): void {
     console.log('Load more button – click handler');
-  }
-
-  ngOnInit() {
-    console.log('ngOnInit', ' app-course-list');
-    this.courseItems = CourseItemEntity.listCreator([
-      {
-        id: 0,
-        title: 'First Title',
-        creationDate: new Date(new Date().setDate(new Date().getDate() + 1)),
-        duration: 60,
-        description: 'some description',
-      },
-      // moved upper in order to see - how orderBy pipe works
-      {
-        id: 2,
-        title: 'Third Title',
-        creationDate: new Date(new Date().setDate(new Date().getDate() - 1)),
-        duration: 44,
-        description: 'Awesome description',
-      },
-      {
-        id: 1,
-        title: 'Second Title',
-        creationDate: new Date('2018-06-11'),
-        duration: 20,
-        description: 'Cool description',
-      },
-    ]);
   }
 }
