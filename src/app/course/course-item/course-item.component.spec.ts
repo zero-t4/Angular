f1768cf3201e091df53ff9870139e11abee7bbb0
Nadir Zhumabaev <Nodirbek_zhumabaev@epam.com>
2018-07-26 17:36:17 +0300

    task/7 fix CourseItemComponent test

--- src/app/course/course-item/course-item.component.spec.ts
+++ src/app/course/course-item/course-item.component.spec.ts
@@ -1,13 +1,15 @@
 import { async } from '@angular/core/testing';
 
 import { CourseItemComponent } from './course-item.component';
+import {CoursesService} from '../../services/courses.service';
 
 describe('CourseItemComponent', () => {
   let component: CourseItemComponent;
 
   beforeEach(async(() => {
-    component = new CourseItemComponent();
+    component = new CourseItemComponent(new CoursesService());
     spyOn(component, 'ngOnInit');
+    spyOn(window, 'confirm').and.returnValue(true);
   }));
 
   it('should create', () => {
