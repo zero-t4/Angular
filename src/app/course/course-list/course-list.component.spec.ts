7e040d8a75df00e517fc3a64bd367c73681ee9b9
Nadir Zhumabaev <Nodirbek_zhumabaev@epam.com>
2018-07-27 14:40:01 +0300

    task/7 fix course-list.component test

--- src/app/course/course-list/course-list.component.spec.ts
+++ src/app/course/course-list/course-list.component.spec.ts
@@ -1,10 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CourseListComponent } from './course-list.component';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {ColorHighlightDirective} from '../../directives/color-bind.directive';
 import {DurationPipe} from '../../pipes/duration.pipe';
 import {OrderByPipe} from '../../pipes/order-by.pipe';
+import {CourseItemComponent} from '../course-item/course-item.component';
 
 describe('CourseListComponent', () => {
   let component: CourseListComponent;
@@ -15,6 +15,7 @@ describe('CourseListComponent', () => {
       declarations: [
         CourseListComponent,
         ColorHighlightDirective,
+        CourseItemComponent,
         DurationPipe,
         OrderByPipe,
       ],
@@ -27,10 +28,6 @@ describe('CourseListComponent', () => {
     fixture = TestBed.createComponent(CourseListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
   });
 
   it('should create', () => {
