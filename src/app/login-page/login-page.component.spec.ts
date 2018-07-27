ae7dd49eac2ddc4341b4ea794e303725c8fc9331
Nadir Zhumabaev <Nodirbek_zhumabaev@epam.com>
2018-07-27 14:38:00 +0300

    task/7 fix login-page.component test

--- src/app/login-page/login-page.component.spec.ts
+++ src/app/login-page/login-page.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginPageComponent } from './login-page.component';
+import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
@@ -8,7 +8,10 @@ describe('LoginPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginPageComponent ]
+      declarations: [
+        LoginPageComponent,
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
     .compileComponents();
   }));
