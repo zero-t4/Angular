1d741cef14d349ca1ffaab2b45834aeccbe584d9
Nadir Zhumabaev <Nodirbek_zhumabaev@epam.com>
2018-07-27 14:36:00 +0300

    task/7 fix courses.service test, removed `.initTestEnvironment` in TestBed

--- src/app/services/courses.service.spec.ts
+++ src/app/services/courses.service.spec.ts
@@ -1,12 +1,9 @@
 import { TestBed, inject } from '@angular/core/testing';
-
 import { CoursesService } from './courses.service';
-import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
 
 describe('CoursesService', () => {
   beforeEach(() => {
     TestBed
-      .initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting())
       .configureTestingModule({
       providers: [CoursesService]
     });
