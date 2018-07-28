80deccdf881a77e5aa0ba0583a0033f10b459169
Nadir <fobasip@gmail.com>
2018-07-28 20:01:09 +0300

    task/7 Implemented navigation to edit page

--- src/app/course/course-item/course-item.component.spec.ts
+++ src/app/course/course-item/course-item.component.spec.ts
@@ -1,16 +1,28 @@
-import { async } from '@angular/core/testing';
-
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import { CourseItemComponent } from './course-item.component';
-import {CoursesService} from '../../services/courses.service';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {DurationPipe} from '../../pipes/duration.pipe';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('CourseItemComponent', () => {
   let component: CourseItemComponent;
+  let fixture: ComponentFixture<CourseItemComponent>;
 
   beforeEach(async(() => {
-    component = new CourseItemComponent(new CoursesService());
-    spyOn(component, 'ngOnInit');
-    spyOn(window, 'confirm').and.returnValue(true);
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [ CourseItemComponent, DurationPipe ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+      .compileComponents();
   }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CourseItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    spyOn(window, 'confirm').and.returnValue(true);
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
