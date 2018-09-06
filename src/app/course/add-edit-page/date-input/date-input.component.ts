3d8fe411269aee803240d78b95816ce574511b9b
Nadir <fobasip@gmail.com>
2018-09-07 02:35:40 +0300

    task/11 made date input as separate component

--- src/app/course/add-edit-page/date-input/date-input.component.ts
+++ src/app/course/add-edit-page/date-input/date-input.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-date-input',
   templateUrl: './date-input.component.html',
-  styleUrls: ['./date-input.component.css']
+  styleUrls: ['./date-input.component.css'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => DateInputComponent),
+    multi: true
+  }]
 })
-export class DateInputComponent implements OnInit {
+export class DateInputComponent implements  OnInit, ControlValueAccessor {
+  @Input()
+  public creationDate: Date;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  public writeValue(creationDate: Date) {
+    this.creationDate = creationDate;
   }
 
+  public registerOnChange() { }
+
+  public registerOnTouched() { }
 }
